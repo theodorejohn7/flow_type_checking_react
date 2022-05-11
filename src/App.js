92463fde9640b254d35da2ab689765a2f54fed8d
theodorejohn7 <theodorejohn.s@10decoders.in>
2022-05-11 14:28:31 +0530

    static checking with flow

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+// @flow
+import   { useState } from "react";
+import * as React from "react";
 
-function App() {
+let randomNumber  = Math.round(Math.random() * 10);
+
+const GuessTheNumber  = ()  : React.Node  => {
+
+  const [value, setValue]   = useState(0);
+  const [results, setResults] = useState("");
+
+  const onClick = () :void => {
+    const userGuess :number = Math.floor(value);  
+    setValue(userGuess);
+    setResults(<p className="alert alert-danger">Pick a number</p>);
+
+    if (userGuess === randomNumber)
+      setResults(<p className="alert alert-success">Correct!</p>);
+    if (userGuess > randomNumber)
+      setResults(<p className="alert alert-warning">High Value, guess again</p>);
+    if (userGuess < randomNumber)
+      setResults(<p className="alert alert-warning">Low Value, guess again</p>);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+    <>
+      <p className="lead">Guess the number between 1 and 10.</p>
+      <input
+        value={value}
+        type="number"
+        onChange={e => setValue(e.target.value)}
+      />
+      <button type="submit" onClick={onClick}> GUESS </button>
+      <br />   <br />
+      <p> {results} </p>
+    </>
+  );};
+export default GuessTheNumber;
